800ad6166f186ae7ba9f1d32312ee56f90f0c5fb
Huy Tran <huytr402@student.liu.se>
2016-01-13 21:20:13 +0100

    add axiom for https requests to github api add github data to user repos and userprofile

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -4,23 +4,30 @@ var Repos = require('./Github/Repos');
 var UserProfile = require('./Github/UserProfile');
 var Notes = require('./Notes/Notes');
 var ReactFireMixin = require('reactfire');
-var Firebase = require('firebase')
+var Firebase = require('firebase');
+var helpers = require('../utils/helpers');
 
 var Profile = React.createClass({
 	mixins:[ReactFireMixin],
 	getInitialState: function(){
 		return {
-			notes:[1,2,3],
-			bio: {
-				name:'HUY TRAN BITCHES'
-			},
-			repos: ['a', 'b', 'c']
+			notes:[],
+			bio: {},
+			repos: []
 		};
 	},
 	componentDidMount: function(){
 		this.ref = new Firebase('https://github-note-taker.firebaseio.com/');
 		var childRef = this.ref.child(this.props.params.username);
 		this.bindAsArray(childRef, 'notes');
+
+		helpers.getGithubInfo(this.props.params.username)
+		.then(function(data){
+			this.setState({
+				bio: data.bio,
+				repos: data.repos
+			})
+		}.bind(this));
 	},
 	componentWillUnmount: function(){
 		this.unbind('notes');
@@ -32,21 +39,21 @@ var Profile = React.createClass({
 	render: function(){
 		return (
 			<div className="row">
-				<div className="col-md-4">
-					<UserProfile username={this.props.params.username} bio={this.state.bio}/>
-				</div>
-				<div className="col-md-4">
-					<Repos username={this.props.params.username} repos={this.state.repos}/>
-				</div>
-				<div className="col-md-4">
-					<Notes
-						username={this.props.params.username} 
-						notes={this.state.notes}
-						addNote={this.handleAddNote}
-					/>
-				</div>
+			<div className="col-md-4">
+			<UserProfile username={this.props.params.username} bio={this.state.bio}/>
 			</div>
-		)
+			<div className="col-md-4">
+			<Repos username={this.props.params.username} repos={this.state.repos}/>
+			</div>
+			<div className="col-md-4">
+			<Notes
+			username={this.props.params.username} 
+			notes={this.state.notes}
+			addNote={this.handleAddNote}
+			/>
+			</div>
+			</div>
+			)
 	}
 });
 
